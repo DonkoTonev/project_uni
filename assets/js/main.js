f2824d3240102274cf773a0f56775a8ffb67e4c4
DonkoTonev <donkotonev04@gmail.com>
2025-04-05 12:25:57 +0300

    Quick fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,3 @@
-
-// courses random color
-
-/*
-var coursesColor = ['#fff','green'];
-var couresBox = document.querySelectorAll('.individual-courses');
-
-for(i= 0; i<couresBox.length;i++){
-   couresBox[i].style.backgroundColor  = coursesColor[Math.floor(Math.random() * coursesColor.length)];
-}*/
-
-// counter testmonial
-
 $('.counter-num').each(function () {
    $(this).prop('Counter',0).animate({
        Counter: $(this).text()
